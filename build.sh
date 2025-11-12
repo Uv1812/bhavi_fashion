01c701741e84d5103a4659e821e1fd504335d7dd
Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>
2025-11-12 08:37:14 +0530

    Update build.sh
    Signed-off-by: Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e  # Stop on error
-echo "Creating superuser..."
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-User.objects.create_superuser('JK', 'uvgothadiya1812@gmail.com', 'jungkook123')" | python manage.py shell
 
 echo "Installing dependencies..."
 pip install -r requirements.txt --no-cache-dir --timeout 300
@@ -17,5 +13,10 @@ python manage.py collectstatic --noinput
 echo "Running deploy checks..."
 python manage.py check --deploy
 
-echo "Starting Gunicorn..."
+echo "Creating superuser..."
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.create_superuser('JK', 'uvgothadiya1812@gmail.com', 'jungkook123')" | python manage.py shell
+
+echo "Starting Server..."
 exec gunicorn bhavi_fashion.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --timeout 300
