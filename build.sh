879bc979fb062fc24e4cf0ae2c1919abbf14323a
Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>
2025-11-12 09:54:51 +0530

    Update build.sh
    Signed-off-by: Urvisha Gothadiya <148183929+Uv1812@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "Running deploy checks..."
 python manage.py check --deploy
 
-echo "Creating superuser..."
-echo "from django.contrib.auth import get_user_model; \
-User = get_user_model(); \
-User.objects.create_superuser('UV', 'uvgothadiya2003@gmail.com', 'jungkook123')" | python manage.py shell
+# echo "Creating superuser..."
+# echo "from django.contrib.auth import get_user_model; \
+# User = get_user_model(); \
+# User.objects.create_superuser('UG', 'uvgothadiya199@gmail.com', 'jungkook123')" | python manage.py shell
 
 echo "Starting Server..."
 exec gunicorn bhavi_fashion.wsgi:application --bind 0.0.0.0:$PORT --workers 1 --timeout 300
